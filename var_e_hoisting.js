46754cf5813623c2c94bf64611a9fb13cce6b00b
Andre Luiz da Silva <andreluiz1013@hotmail.com>
2016-09-15 13:57:02 -0300

    Var and Hoisting

---
+++ var_e_hoisting.js
@@ -0,0 +1,16 @@
+var percentualImposto = 0.15;
+var valor = 100;
+var totalImposto = valor * percentualImposto;
+
+console.log('Total imposto a pagar', totalImposto);
+
+//HOISTING 
+//Leva todas as váriaveis para cima, mas não as atribuições
+
+//var percentualImposto, valor, totalImposto;
+//percentualImposto = 0.15;
+//totalImposto = valor * percentualImposto;
+
+//console.log('Total imposto a pagar', totalImposto);
+
+//valor = 100;

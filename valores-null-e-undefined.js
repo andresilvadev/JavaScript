7ce9bc7dbe5e4815b70e20bfe9183944da4e7686
Andre Luiz da Silva <andreluiz1013@hotmail.com>
2016-09-15 18:40:03 -0300

    Value Null e Undefined

---
+++ valores-null-e-undefined.js
@@ -0,0 +1,15 @@
+//Null e Undefinid, idenfitica ausência de um valor
+
+//var x = 10;
+var x;
+console.log(x);
+
+var y = null;
+console.log(y);
+
+
+if(x != null){
+	console.log('Existe um valor em x:', x);
+}else{
+	console.log('x não tem um valor útil');
+}

0e8b1ee5d764906589e8d7b8c4daf8cfbdc75f08
Andre Luiz da Silva <andreluiz1013@hotmail.com>
2016-09-15 20:36:11 -0300

    Mathematical Functions

---
+++ funcoes-matematicas.js
@@ -0,0 +1,25 @@
+var menorNota = Math.min(3, 5, 1, 6, 10);
+console.log('Menor nota é:', menorNota);
+
+
+var maiorNota = Math.max(3, 5, 1, 6, 10);
+console.log('Maior nota é:', maiorNota);
+
+
+var temperatura = Math.round(22.7);
+console.log('A temperatura é:', temperatura);
+
+
+var valorAleatorio = Math.random();
+console.log('O valor aleatório é:', valorAleatorio);
+
+var numero = valorAleatorio * 60;
+console.log('Numero', numero);
+
+
+var numeroMega = Math.random(4.9);
+console.log('O número da mega é:', numeroMega);
+
+
+var numberMega = Math.floor(numero) + 1;
+console.log('O número da mega é:', numberMega);

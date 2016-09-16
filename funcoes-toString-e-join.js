ad73fcc6a29a961aa2185d99ead03ea3ecbcbc51
Andre Luiz da Silva <andreluiz1013@hotmail.com>
2016-09-16 01:01:05 -0300

    Functions toString(), and join()

---
+++ funcoes-toString-e-join.js
@@ -0,0 +1,10 @@
+var carros = [
+	'Fox',
+	'Celta',
+	'Gol',
+	'Palio'
+];
+
+console.log(carros.toString()); //Mostra separado por virgulas
+
+console.log(carros.join(' - ')); //Mostra separado com o que eu queira separar, no caso >> - <<

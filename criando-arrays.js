1c7443d4b5999131b5770226710351bfe91941e8
Andre Luiz da Silva <andreluiz1013@hotmail.com>
2016-09-16 00:36:56 -0300

    Creating Arrays

---
+++ criando-arrays.js
@@ -0,0 +1,15 @@
+var emails = [];
+
+emails[0] = 'joao@email.com';
+emails[1] = 'maria@email.com';
+
+console.log('emails[0]', emails[0]);
+
+
+var nomes = [
+	'João',
+	'Maria',
+	'José',
+];
+console.log('Quentos nomes?', nomes.length);
+

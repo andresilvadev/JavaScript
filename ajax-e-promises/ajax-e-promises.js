a404603f6e3d860ad823b31e49825b2c34b8e2c3
Andre Luiz da Silva <andreluiz1013@hotmail.com>
2016-09-16 15:02:09 -0300

    DOM Manipulation

--- ajax-e-promises/ajax-e-promises.js
+++ ajax-e-promises/ajax-e-promises.js
@@ -3,14 +3,22 @@ $(function(){
 	var resposta = $.ajax({
 		url: 'http://localhost:8080/estados',
 		method: 'GET',
-		dataType: 'jsonp', //Somente por que não estamos no mesmo domínio
+		dataType: 'jsonp' //Somente por que não estamos no mesmo domínio
 	});
 
 	resposta.done(function(estados){
 
+		var comboEstado = $('#combo-estado');
+			//comboEstado.empty();
+			comboEstado.html('<option> Selecione estado </option>');
+
 		estados.forEach( function(estado) {
 			
-			console.log('estado');
+			// Var optionEstado será um elemente do tipo option com o value uf e nome do estado
+			var optionEstado = $('<option>').val(estado.uf).text(estado.nome);
+
+			//comboEstado recebe o option de estado
+			comboEstado.append(optionEstado);
 
 		});
 
